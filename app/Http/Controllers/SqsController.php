bc39f61db06f1215f45a5db15425e442e843df11
HAMZA <hamza.nadeem@innovotechnologies.com>
2020-10-13 20:09:07 +0500

    sqs implementation

---
+++ app/Http/Controllers/SqsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+
+class SqsController extends Controller
+{
+
+	/**
+	* [getServerList description]
+	* @return string [description]
+	*/
+	public function getServerList(): array
+	{
+		
+	}
+
+}
